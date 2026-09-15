df8f5a7276ebd1216ea0990897b02c14a177e2a6
Ditmar Riksten <90682255+DitmarR@users.noreply.github.com>
2026-09-15 09:49:08 +0200

    created fields and grid

--- Test.java
+++ Test.java
@@ -2,6 +2,21 @@ package TicyTacyToe;
 
 public class Test {
     public static void main(String[] args) {
+        Intro();
+        String[][] board = createGrid();
+
+        for (String[] row : board) {
+            for (String cell : row) {
+                System.out.print("[" + cell + "]");
+            }
+            System.out.println();
+        }
+
+        System.out.println(board);
+    }
+
+
+    public static void Intro() {
         System.out.println("Gday welcome to Ticy Tacy Toe!");
         System.out.println("Rules: Player 1 and player 2, represented by X and O, take turns \"\n" +
                 "          \"marking the spaces in a 3*3 grid. The player who succeeds in placing \"\n" +
@@ -9,5 +24,17 @@ public class Test {
 
         System.out.println("Press enter to continue. :)");
     }
+
+    public static String[][] createGrid() {
+        String[][] board = {
+                {" ", " ", " "},
+                {" ", " ", " "},
+                {" ", " ", " "}
+        };
+        System.out.println("here's the playboard");
+
+        return board;
+    }
+
 
 }
