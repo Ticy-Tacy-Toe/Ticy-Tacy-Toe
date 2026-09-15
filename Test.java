68e44099f8d43576a72aa7723bb8cffe35b14c46
Ditmar Riksten <90682255+DitmarR@users.noreply.github.com>
2026-09-15 09:28:53 +0200

    finished intro code

--- Test.java
+++ Test.java
@@ -1,3 +1,5 @@
+
+
 public class Test {
 
     public static void main(String[] args) {
@@ -5,5 +7,7 @@ public class Test {
         System.out.println("Rules: Player 1 and player 2, represented by X and O, take turns \"\n" +
                 "          \"marking the spaces in a 3*3 grid. The player who succeeds in placing \"\n" +
                 "          \"three of their marks in a horizontal, vertical, or diagonal row wins");
+
+        System.out.println("Press enter to continue. :)");
     }
 }
