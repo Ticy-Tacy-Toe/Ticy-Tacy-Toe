2b5a33aa2d659f8367c4f2c77d1307a2b8a65fad
Ditmar Riksten <90682255+DitmarR@users.noreply.github.com>
2026-09-15 09:35:57 +0200

    Initial commit for Dev branch

---
+++ Test.java
@@ -0,0 +1,13 @@
+package TicyTacyToe;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Gday welcome to Ticy Tacy Toe!");
+        System.out.println("Rules: Player 1 and player 2, represented by X and O, take turns \"\n" +
+                "          \"marking the spaces in a 3*3 grid. The player who succeeds in placing \"\n" +
+                "          \"three of their marks in a horizontal, vertical, or diagonal row wins");
+
+        System.out.println("Press enter to continue. :)");
+    }
+
+}
