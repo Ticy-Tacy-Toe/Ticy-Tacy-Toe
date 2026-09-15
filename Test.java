31d4b985857cfe67333e87176f5cc7ec951e2d17
Ditmar Riksten <90682255+DitmarR@users.noreply.github.com>
2026-09-15 10:41:05 +0200

    fixed intro function

--- Test.java
+++ Test.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Test {
     public static void main(String[] args) {
-        Intro();
+        intro();
 
         String[][] board = createGrid();
 
@@ -25,11 +25,11 @@ public class Test {
     }
 
 
-    public static void Intro() {
+    public static void intro() {
         System.out.println("Gday welcome to Ticy Tacy Toe!");
-        System.out.println("Rules: Player 1 and player 2, represented by X and O, take turns \"\n" +
-                "          \"marking the spaces in a 3*3 grid. The player who succeeds in placing \"\n" +
-                "          \"three of their marks in a horizontal, vertical, or diagonal row wins");
+        System.out.println("Rules: Player 1 and player 2, represented by X and O, take turns \n" +
+                "          marking the spaces in a 3*3 grid. The player who succeeds in placing \n" +
+                "          three of their marks in a horizontal, vertical, or diagonal row wins");
 
         System.out.println("Press enter to continue. :)");
     }
