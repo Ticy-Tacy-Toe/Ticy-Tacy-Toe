0d9e8d509f8ff1d8b64e51ff9288cee0f72568ca
Ditmar Riksten <90682255+DitmarR@users.noreply.github.com>
2026-09-17 14:44:23 +0200

    AI makes choice

---
+++ AI.java
@@ -0,0 +1,18 @@
+package TicyTacyToe;
+
+import java.util.Arrays;
+
+public class AI {
+    public static void main() {
+
+        System.out.println(Arrays.toString(aiChoice()));
+    }
+
+    public static int[] aiChoice() {
+        int random = (int) (Math.random() * 3);
+        int random2 = (int) (Math.random() * 3);
+
+        return new int[]{random, random2};
+    }
+
+}
